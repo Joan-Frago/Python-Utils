8bd0c718a57450f238d411684db229e9ca175d12
Joan Frago <jfragojimenez@gmail.com>
2024-11-13 13:05:21 -0500

    adding utils

--- utils.py
+++ utils.py
@@ -1,15 +1,80 @@
+import requests
+import csv
+from datetime import datetime
+
 class Utils:
-    def __init__(self, afile:None, acontent:None):
+    def __init__(self, afile:str = None, acontent:str = None, aUrl:str = None, headers:dict = None, body:dict = None, dateTime = None, timestamp:str = None, timeZone = None):
         self.afile = afile
+        self.acontent = acontent
+        self.aUrl = aUrl
+        self.headers = headers
+        self.body = body
+        self.dateTime = dateTime
+        self.timestamp = timestamp
+        self.timeZone = timeZone
     
     # Read a File
     def readFile(self):
-        with open(self.afile,"r") as file:
-            content=file.read()
-        return content
-    
+        try:
+            if self.afile != "":
+                with open(self.afile,"r") as file:
+                    content=file.read()
+                return content
+            else:
+                return "Please provide a path for file reading"
+        except IOError as e:
+            return f"Could not read {self.afile} contents due to {e}"
+
     # Write a File
     def writeFile(self):
-        with open(self.afile,"w") as file:
-            file.write(self.acontent)
-            
+        try:
+            if self.afile != "":
+                with open(self.afile,"w") as file:
+                    file.write(self.acontent)
+            else:
+                return "Please provide a path for file reading"
+        except IOError as e:
+            return f"Could not write to {self.afile} due to {e}"
+
+    def getJsonData(self):
+        try:
+            response = requests.get(self.aUrl, self.headers)
+            if response.status_code == 200:
+                data = response.json()
+            else:
+                return f"Error: {response.status_code}"
+            return data
+        except Exception as e:
+            return f"ERROR: {e}"
+        
+    def postJsonData(self):
+        try:
+            response = requests.post(self.aUrl, json=self.body)
+            if response.status_code == 200:
+                data = response.json()
+            else:
+                return f"Error: {response.status_code}"
+            return data
+        except Exception as e:
+            return f"ERROR: {e}"
+    
+    def readCSV(self):
+        try:
+            csvDict = []
+            with open(self.afile,mode="r") as file:
+                csv2dict = csv.DictReader(file,delimiter=",")
+                for row in csv2dict:
+                    csvDict.append(row)
+            return csvDict
+
+        except Exception as e:
+            return e
+    
+    def Date2Timestamp(self):
+        timestamp = int(datetime.timestamp(self.dateTime))
+
+        return timestamp
+
+    def Timestamp2Date(self):
+        date = datetime.fromtimestamp(self.timestamp,tz=self.timeZone)
+        return date
