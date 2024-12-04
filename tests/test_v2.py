a4294bb1921d5622825c8d3df986258d5895deb4
Joan Frago <jfragojimenez@gmail.com>
2024-12-04 19:42:15 +0100

    tests

--- tests/test_v2.py
+++ tests/test_v2.py
@@ -1,14 +1,19 @@
 import sys
 import os
 from datetime import datetime
+import random
 
 # Import the module
-sys.path.append(os.path.dirname(os.path.dirname("utils.py")))
+sys.path.append(os.path.dirname(os.path.dirname("utils/utils.py")))
 from utils import *
 
-iFile="testFile.txt"
+"""iFile="testFile.txt"
 iContent="djfaoijgjds ----- test -----"
 writeFile(iFile,iContent,newLine=True)
 
 outFile = readFile(iFile)
-print(outFile)
+print(outFile)"""
+
+num = random.randint(1,100000)
+prime = IsPrime(num)
+print(prime)
